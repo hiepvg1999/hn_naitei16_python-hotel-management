592cee72d2157c529bfdb92a262bf3756870e3b1
LongNguyenVu181 <longnguyenvu181@gmail.com>
2021-08-15 15:10:24 +0700

    Register-login

---
+++ hotel/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from phonenumber_field.modelfields import PhoneNumberField
+from .models import User
+
+class NewUserForm(UserCreationForm):
+    
+    username = forms.CharField(
+        label=_('User Name'),
+        required=True,
+    )
+    first_name = forms.CharField(
+        label=_('First Name'),
+        required=True,
+    )
+    last_name = forms.CharField(
+        label=_('Last Name'),
+        required=True,
+    )
+    email = forms.EmailField(
+        label=_('Email'),
+        widget=forms.EmailInput,
+        required=True,
+    )
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput,
+        required=True
+    )
+    password2 = forms.CharField(
+        label=_('Conrfirm Password'),
+        widget=forms.PasswordInput,
+        required=True
+    )
+    phoneNumber = PhoneNumberField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'phoneNumber', 'password1', 'password2')
+        
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username__iexact=username).exists():
+            raise forms.ValidationError(_("Username is already in use."))
+        return username
--- hotel/views.py
+++ hotel/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .forms import NewUserForm
+
+# Create your views here.
+def user_register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    else: 
+        form = NewUserForm()
+    return render (request=request, template_name="registration/register.html", context={"form":form})
+
+def index(request):
+    return render (request=request, template_name="index.html")

4a90197dfc88e28c56aa482770ac6b035ca3a74f
hiepvg1999 <hiepbkcntt@gmail.com>
2021-08-21 10:48:36 +0700

    Search user

--- hotel/urls.py
+++ hotel/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('user-profile/', views.UserProfileView.as_view(), name= 'user-profile'),
     path('edit_profile/', views.edit_profile, name = 'edit-profile'),
+    path('list-users/', views.list_users, name = 'list-users'),
 ]

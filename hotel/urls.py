43ff70b9568467c10511042bb484ece8976c735a
hiepvg1999 <hiepbkcntt@gmail.com>
2021-08-25 17:28:39 +0700

    Payment

--- hotel/urls.py
+++ hotel/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('edit_profile/', views.edit_profile, name = 'edit-profile'),
     path('list-users/', views.list_users, name = 'list-users'),
     path('list-bookings/', views.list_bookings_staff, name = "list-bookings-staff"),
-    path('statistic_page/', views.statistic_page, name = 'statistic_page')
+    path('statistic_page/', views.statistic_page, name = 'statistic_page'),
+    path('payment/<uuid:booking_id>', views.payment, name= 'payment')
 ]
